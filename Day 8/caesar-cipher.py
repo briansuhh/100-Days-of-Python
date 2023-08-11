4e3cca4ddf581c619235e7d05f16416725902832
ayumi <brianmaysebastian@gmail.com>
2023-08-12 02:06:04 +0800

    update caesar-cipher code

--- Day 8/caesar-cipher.py
+++ Day 8/caesar-cipher.py
@@ -1,17 +1,30 @@
 import string 
 characters = list(string.ascii_lowercase)
+while 1:
+    choice = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    message = input("Type your message:\n")
+    shift = int(input("Type the shift number:\n"))
 
-# choice = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-message = input("Type your message:\n")
-shift = int(input("Type the shift number:\n"))
+    # get the index of all the letters on the message based on the alphabet and add the shift number
+    result = ''
+    for char in message:
+        if char == " ":
+            result += " "
+        elif char in characters:
+            index = characters.index(char)
+            if choice == "encode":
+                index = (index + shift) % 26
+                print(index)
+            if choice == "decode":
+                index = (index - shift) % 26
+            result += characters[index]
+        else:
+            result += char
 
-# get the index of all the letters on the message based on the alphabet and add the shift number
-indexes = []
-result = []
-for i in range(0, len(message)):
-    indexes.append(characters.index(message[i]))
-    indexes[i] += shift
-    result.append(characters[indexes[i]])
+    print(f"Here's the result: {result}")
 
-result = ''.join(str (item) for item in result)
-print(f"Here's the encoded result:{result}")
+    user_choice = input("Do you want to try again? (yes/no) ")
+    if user_choice == "yes":
+        continue
+    else:
+        break
