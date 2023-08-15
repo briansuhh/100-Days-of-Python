8fe589a789433a70ef5b4abebecb7639ef9d0e83
ayumi <brianmaysebastian@gmail.com>
2023-08-16 00:55:07 +0800

    rename

---
+++ Day 9/exercises.py
@@ -0,0 +1,63 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+for key in student_scores:
+    if student_scores[key] < 70:
+        student_grades[key] = "Fail"
+    elif student_scores[key] < 80:
+        student_grades[key] = "Acceptable"
+    elif student_scores[key] < 90:
+        student_grades[key] = "Exceeds Expectations"
+    else:
+        student_grades[key] = "Outstanding"
+    
+# 🚨 Don't change the code below 👇
+print(student_grades)
+
+
+
+
+
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+
+def add_new_country(a, b, c):
+    travel_log.append({
+        "country": a,
+        "visits": b,
+        "cities": c,
+    })
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
+
+
+
+
+
+
