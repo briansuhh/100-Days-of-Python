212c99026ed455bad02bda03ca2ed999216d6e87
ayumi <brianmaysebastian@gmail.com>
2023-08-16 12:24:41 +0800

    add exercises

---
+++ Day 10/exercises.py
@@ -0,0 +1,27 @@
+def is_leap(year):
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        return True
+    else:
+        return False
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+    if is_leap(year):
+        month_days[1] = 29
+    # Return the number of days for the said month
+    result = month_days[month - 1]  
+    return result
+  
+# 🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+
+
+
+
+
+
+
