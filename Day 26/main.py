69c8e1e81901189dcdaaf4c9f8dbdd8582719ba0
ayumi <brianmaysebastian@gmail.com>
2023-09-26 17:12:29 +0800

    update main.py -added error handling in the main code

--- Day 26/main.py
+++ Day 26/main.py
@@ -8,8 +8,15 @@ data = pandas.read_csv("Day 26/nato_phonetic_alphabet.csv")
 
 nato_aplhabet = {row.letter:row.code for index, row in data.iterrows()}
 
-input_word = input("Enter a word: ").upper()
 
-output_list = [nato_aplhabet[letter] for letter in input_word]
+def nato_alphabet():
+    input_word = input("Enter a word: ").upper()
+    try:
+        output_list = [nato_aplhabet[letter] for letter in input_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        nato_alphabet()
+    else:
+        print(output_list)
 
-print(output_list)
+nato_alphabet()
