735d47d14f225f0c16bba2d890c50ac93cb08cb3
ayumi <brianmaysebastian@gmail.com>
2023-10-05 00:22:06 +0800

    add day 33 files -kanye quotes using gui

---
+++ Day 33/README.md
@@ -0,0 +1,9 @@
+
+# Project: ISS Overhead Notifier
+- Instructions:<br>
+Create a notifier for the ISS using an API.
+
+- [ISS Overhead Notifier Code](main.py)
+
+- Output:<br>
+![ISS Overhead Notifier](../assets/img/33_project.png)
---
+++ Day 33/kanye_quotes/main.py
@@ -0,0 +1,33 @@
+from tkinter import *
+import requests
+
+
+def get_quote():
+    try:
+        request = requests.get(url="https://api.kanye.rest")
+        request.raise_for_status()
+
+        data = request.json()
+        quote = data["quote"]
+        canvas.itemconfig(quote_text, text=quote)
+    except Exception as error:
+        print(f"Error: {error}")
+
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="Day 33/kanye_quotes/background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 22, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="Day 33/kanye_quotes/kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+
+
+window.mainloop()
